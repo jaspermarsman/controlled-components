b76d21c3de373c64ac4ef9d57eb017a12d07e9f8
jaspermarsman <jaspermarsman@gmail.com>
2021-05-20 22:35:09 +0200

    inputs als controlled components aangemaakt

--- src/App.js
+++ src/App.js
@@ -2,9 +2,61 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [nameValue, setNameValue] = React.useState("");
+    const [ageValue, setAgeValue] = React.useState();
+    const [remarksValue, setRemarksValue] = React.useState();
+
   return (
-    <div>
-      Maak hier jouw formulier!
+    <div className={"divstyle"}>
+        <form>
+            <fieldset>
+                <legend>Gegevens</legend>
+                <label htmlFor={"name"}>Naam
+                    <input
+                        type={"text"}
+                        id={"name"}
+                        name={"name"}
+                        value={nameValue}
+                        onChange={(e) => setNameValue(e.target.value)}
+                    >
+                    </input>
+                </label>
+                <label htmlFor={"age"}>Leeftijd
+                    <input
+                        type={"number"}
+                        id={"age"}
+                        name={"age"}
+                        value={ageValue}
+                        onChange={(e) => setAgeValue(e.target.value)}
+                        >
+                    </input>
+                </label>
+            </fieldset>
+            <fieldset>
+                <legend>Jouw review</legend>
+                <label htmlFor={"source"}>Hoe heb je dit recept gevonden?
+                    <select
+                    id={"source"}
+                    name={"source"}
+                    >
+                        <option value={"anders"}>Anders</option>
+                        <option value={"vriend"}>Vriend</option>
+                        <option value={"google"}>Google</option>
+                        <option value={"advertentie"}>Advertentie</option>
+                    </select>
+                </label>
+                <label htmlFor={"remarks"}>Opmerkingen:
+                    <textarea
+                        id={"remarks"}
+                        name={"remarks"}
+                        placeholder={"Wat vond je van het recept?"}
+                        value={remarksValue}
+                        onChange={(e) => setRemarksValue(e.target.value)}
+                    >
+                    </textarea>
+                </label>
+            </fieldset>
+        </form>
     </div>
   );
 }
